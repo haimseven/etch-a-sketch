81d371117ad9b979760b9619c954859fafa1efec
Sven Nesfeder <154921029+haimseven@users.noreply.github.com>
2024-02-07 19:33:34 -0500

    Added reset button to reset grid

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const container = document.querySelector(".container");
 
+function resetGrid(){
+    container.innerHTML = "";
+
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", function() {
+                square.classList.add("hovered");
+            });
+            container.appendChild(square);
+        }
+    }
+    
+}
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
